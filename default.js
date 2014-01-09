98ccb1c62e76c49bf1779a580fbfc22d0628357f
JeffJacobson <JeffJacobson@github.fake>
2014-01-09 15:53:05 -0800

    default.htm now lists a table of agencies in WA.

--- default.js
+++ default.js
@@ -7,8 +7,37 @@ require(["gtfs-exchange"], function (Agencies) {
 		return val && val.country === "United States" && val.state === "Washington";
 	}
 
+	function createAgencyTable(/**Object[]*/ agencies) {
+		var table, propNames, i, l, row, cell;
+		if (agencies && agencies.length) {
+			table = document.createElement("table");
+			table.createTBody();
+			// Get agency property names.
+			propNames = Object.getOwnPropertyNames(agencies[0]);
+
+			for (i = 0, l = agencies.length; i < l; i++) {
+				row = table.insertRow(i);
+				for (var j = 0, jl = propNames.length; j < jl; j += 1) {
+					cell = row.insertCell(j);
+					cell.textContent = String(agencies[i][propNames[j]]);
+				}
+			}
+
+			table.createTHead();
+			row = table.insertRow(0);
+			for (i = 0, l = propNames.length; i < l; i += 1) {
+				cell = row.insertCell(i);
+				cell.textContent = propNames[i];
+			}
+		}
+
+		return table;
+		
+	}
+
 	function handleAgencyList(/**{string}*/ json) {
 		var agencies, agencyResponse = Agencies.parseAgencyResponse(json);
+
 
 		/** Use with Array.filter() filter out non-WA agencies.
 		*/
@@ -16,7 +45,7 @@ require(["gtfs-exchange"], function (Agencies) {
 		if (agencyResponse.data) {
 			agencies = agencyResponse.data;
 			agencies = agencies.filter(isInWA);
-			console.log(agencies);
+			document.body.appendChild(createAgencyTable(agencies));
 		}
 	}
 
