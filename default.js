fd24597f16ef1c99fcec446b3c1fcf6ce08c40b9
JeffJacobson <JeffJacobson@github.fake>
2014-01-09 14:06:42 -0800

    Removed class definitions in gtfs-exchange.js (nee agencies.js).

--- default.js
+++ default.js
@@ -1,4 +1,17 @@
-﻿require(["agencies", "//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text.min.js!agencies.txt"], function (Agencies, agencyResponse) {
-	var agencies = Agencies.parseAgencyResponse(agencyResponse);
-	console.log(agencies);
+﻿require(["gtfs-exchange", "//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text.min.js!agencies.txt"], function (Agencies, agencyResponse) {
+	"use strict";
+	var agencies;
+	agencyResponse = Agencies.parseAgencyResponse(agencyResponse);
+
+	/** Use with Array.filter() filter out non-WA agencies.
+	*/
+	function isInWA(val) {
+		return val && val.country === "United States" && val.state === "Washington";
+	}
+
+	if (agencyResponse.data) {
+		agencies = agencyResponse.data;
+		agencies = agencies.filter(isInWA);
+		console.log(agencies);
+	}
 });
---
+++ gtfs-exchange.js
@@ -0,0 +1,51 @@
+﻿/*global define, module*/
+
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(factory);
+	////} else if (typeof exports === 'object') {
+	////	// Node. Does not work with strict CommonJS, but
+	////	// only CommonJS-like enviroments that support module.exports,
+	////	// like Node.
+	////	module.exports = factory();
+	} else {
+		// Browser globals (root is window)
+		root.gtfsx = factory();
+	}
+}(this, function () {
+
+	/** Converts a number representing a date returned from GTFS-Exchange into a Date object.
+	 * @returns {Date}
+	 */
+	function convertDateToDateObject(/*{number}*/ n) {
+		return new Date(n * 1000);
+	}
+
+	/** For use with JSON.parse(). Dates numbers will be converted to Date objects.
+	 * @param {string} k - The property name.
+	 * @param {?(number|string|boolean|Array|Object)} v - The value of the property.
+	 * @returns {?(number|string|boolean|Array|Object|Date)}
+	 */
+	function agencyReviver(k, v) {
+		var output = v;
+		if ((k === "date_added" || k === "date_last_updated") && v) {
+			output = new convertDateToDateObject(v);
+		}
+		return output;
+	}
+
+	/** Parses a JSON string into an object. 
+	 */
+	function parseAgencyResponse(/**{String}*/ json) {
+		return JSON.parse(json, agencyReviver);
+	}
+
+	// Just return a value to define the module export.
+	// This example returns an object, but the module
+	// can return a function as the exported value.
+	return {
+		//Agency: Agency,
+		parseAgencyResponse: parseAgencyResponse
+	};
+}));
