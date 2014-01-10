42d21314e35825ad108779d4ac15522e96b045a5
JeffJacobson <JeffJacobson@github.fake>
2014-01-09 17:06:56 -0800

    Modified table formatting on default.htm.

--- default.js
+++ default.js
@@ -8,15 +8,30 @@ require(["gtfs-exchange"], function (Agencies) {
 	}
 
 	function createAgencyTable(/**Object[]*/ agencies) {
-		var table, propNames, insertCell, row;
+		var table, insertCell, row, propNames, thead;
+
+		propNames = [
+			"name",
+			"area",
+			//"country",
+			//"state",
+			"url",
+			"dataexchange_url",
+			"feed_baseurl",
+			"license_url",
+			"date_added",
+			"date_last_updated",
+			"is_official",
+			"dataexchange_id"
+		];
 
 
 
 		if (agencies && agencies.length) {
 			table = document.createElement("table");
 			table.createTBody();
-			// Get agency property names.
-			propNames = Object.getOwnPropertyNames(agencies[0]);
+			////// Get agency property names.
+			////propNames = Object.getOwnPropertyNames(agencies[0]);
 
 			/** Inserts a cell into a row.
 			 * @this {Object} Properties are row and agency.
@@ -31,7 +46,8 @@ require(["gtfs-exchange"], function (Agencies) {
 					a.href = value;
 					cell.appendChild(a);
 				} else if(dateRe.test(propName)) {
-					cell.textContent = value.toLocaleString();
+					cell.textContent = value.toISOString().slice(0, 10);
+					cell.title = value.toLocaleString();
 				} else if (typeof value === "boolean") {
 					cb = document.createElement("input");
 					cb.type = "checkbox";
@@ -54,12 +70,13 @@ require(["gtfs-exchange"], function (Agencies) {
 			});
 
 			// Add the header
-			table.createTHead();
-			row = table.insertRow(0);
+			thead = table.createTHead();
+			row = thead.insertRow(0);
 
-			propNames.forEach(function (propName, i) {
-				var cell = row.insertCell(i);
-				cell.textContent = propName;
+			propNames.forEach(function (propName) {
+				var cell = document.createElement("th");
+				cell.textContent = propName.replace(/_/g, " ");
+				row.appendChild(cell);
 			});
 		}
 
